73cc62ed51ae02bb34b6a3fa7aa2b83916dd7b0f
wanxing <wanxing2@illinois.edu>
2023-02-08 00:25:17 -0500

    prototype

--- network_viz.py
+++ network_viz.py
@@ -21,6 +21,7 @@ import networkx
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from operator import itemgetter
 # from bokeh.io import output_notebook, show, save
 from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
 from bokeh.plotting import figure, curdoc, from_networkx
@@ -46,8 +47,8 @@ df_cat['domains'] = df_cat['domain'].apply(lambda x: x.split('/')[2].replace('ww
 
 url_class = dict(zip(df_cat['domains'],df_cat['domain']))
 category_class = dict(zip(df_cat['domains'],df_cat['Category']))
-df['source_category'] = df.merge(df_cat, how='left', left_on='source', right_on='domains')['Category']
-df['target_category'] = df.merge(df_cat, how='left', left_on='target', right_on='domains')['Category']
+df['source_category'] = df['source'].map(category_class)
+df['target_category'] = df['target'].map(category_class)
 
 
 """#Network(interactive)"""
@@ -55,7 +56,7 @@ from bokeh.core.enums import SizingMode
 from bokeh.io import show
 from bokeh.models import CustomJS, MultiChoice
 from bokeh.io.output import output_file
-from bokeh.models import Button, CustomJS, Div, CheckboxButtonGroup, TapTool, OpenURL
+from bokeh.models import Button, CustomJS, Div, CheckboxButtonGroup, TapTool, OpenURL, Select
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
 from bokeh.models import CDSView, ColumnDataSource, GroupFilter
@@ -132,11 +133,6 @@ def callback(attr, old, new):
     print("source",source_selected)
     print("target",target_selected)
     source =  df[(df["target_category"].isin(target_selected)) & (df['source_category'].isin(source_selected))]
-    # # new_data = dict()
-    # # new_x = filtered["Year"].unique()
-    # # new_y = filtered.groupby("Year")["Value"].sum().values
-    # # new_data["x"] = new_x
-    # # new_data["y"] = new_y
     print("updated",source)
     G = networkx.from_pandas_edgelist(source, 'source', 'target', edge_attr=['weight'], create_using=networkx.DiGraph())
 
@@ -199,88 +195,10 @@ def callback(attr, old, new):
                 column(target_cat, target_select_all, target_select_none, target_checkbox_button_group),
                 sizing_mode='stretch_both')
     layout.children[2] = new_plot
-# def source_callback(attr, old, new):
-#     new_plot = figure(tooltips = HOVER_TOOLTIPS,
-#               tools="pan,wheel_zoom,save,reset,tap", active_scroll='wheel_zoom',
-#               x_range=Range1d(-15.1, 15.1), y_range=Range1d(-15.1, 15.1), 
-#               plot_width=1000, 
-#               title=title)
-#     source_selected = [source_checkbox_button_group.labels[i] for i in source_checkbox_button_group.active]
-#     target_selected = [target_checkbox_button_group.labels[i] for i in target_checkbox_button_group.active]
-#     print("source",source_selected, target_selected)
-#     source =  df[df["source_category"].isin(source_selected) and df['target_category'].isin(target_selected)]
-#     # # new_data = dict()
-#     # # new_x = filtered["Year"].unique()
-#     # # new_y = filtered.groupby("Year")["Value"].sum().values
-#     # # new_data["x"] = new_x
-#     # # new_data["y"] = new_y
-#     print("source",source)
-#     G = networkx.from_pandas_edgelist(source, 'source', 'target', edge_attr=['weight'], create_using=networkx.DiGraph())
-
-#     #Calculate degree for each node and add as node attribute
-#     degrees = dict(networkx.degree(G))
-#     networkx.set_node_attributes(G, name='degree', values=degrees)
-
-#     #Slightly adjust degree so that the nodes with very small degrees are still visible
-#     number_to_adjust_by = 5
-#     adjusted_node_size = dict([(node, degree+number_to_adjust_by) for node, degree in networkx.degree(G)])
-#     networkx.set_node_attributes(G, name='adjusted_node_size', values=adjusted_node_size)
-
-#     # communities = community.greedy_modularity_communities(G)
-#     # #Add modularity class and color as attributes to network graph
-
-#     # # Create empty dictionaries
-#     # modularity_class = {}
-#     # modularity_color = {}
-#     # #Loop through each community in the network
-#     # for community_number, community in enumerate(communities):
-#     #     #For each member of the community, add their community number and a distinct color
-#     #     for name in community: 
-#     #         modularity_class[name] = community_number
-#     #         modularity_color[name] = Spectral8[community_number]
-
-#     # Add modularity class, color, URLs as attributes from the network above
-#     networkx.set_node_attributes(G, modularity_class, 'modularity_class')
-#     networkx.set_node_attributes(G, modularity_color, 'modularity_color')
-#     networkx.set_node_attributes(G, url_class, "URL")
-#     networkx.set_node_attributes(G, category_class, "category")
-#     # Configure tap tool
-#     taptool = new_plot.select(type=TapTool)
-#     taptool.callback = OpenURL(url="@URL")
-#     network_graph = from_networkx(G, networkx.spring_layout, scale=15, center=(0, 0))
-#     #Create newnetwork graph from filtered data
-#     #Set node sizes and colors according to node degree (color as category from attribute)
-#     network_graph.node_renderer.glyph = Circle(size=size_by_this_attribute, fill_color=color_by_this_attribute)
-#     #Set node highlight colors
-#     network_graph.node_renderer.hover_glyph = Circle(size=size_by_this_attribute, fill_color=node_highlight_color, line_width=2)
-#     network_graph.node_renderer.selection_glyph = Circle(size=size_by_this_attribute, fill_color=node_highlight_color, line_width=2)
-#     #Set edge opacity and width
-#     network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.3, line_width=1)
-#     #Set edge highlight colors
-#     network_graph.edge_renderer.selection_glyph = MultiLine(line_color=edge_highlight_color, line_width=2)
-#     network_graph.edge_renderer.hover_glyph = MultiLine(line_color=edge_highlight_color, line_width=2)
-#     #Highlight nodes and edges
-#     network_graph.selection_policy = NodesAndLinkedEdges()
-#     network_graph.inspection_policy = NodesAndLinkedEdges()
-#     new_plot.renderers.append(network_graph)
-#     #Add Labels
-#     x, y = zip(*network_graph.layout_provider.graph_layout.values())
-#     node_labels = list(G.nodes())
-#     label_source = ColumnDataSource({'x': x, 'y': y, 'name': [node_labels[i] for i in range(len(x))]})
-#     labels = LabelSet(x='x', y='y', text='name', source=label_source, background_fill_color='white', text_font_size='10px', background_fill_alpha=.8)
-#     new_plot.renderers.append(labels)
-#     # show(column(group, plot))
-#     # output_file('network.html')
-#     #save(plot, filename=f"{title}.html")
-#     group = row(column(source_cat, source_select_all, source_select_none, source_checkbox_button_group),
-#                 column(target_cat, target_select_all, target_select_none, target_checkbox_button_group),
-#                 sizing_mode='stretch_both')
-#     layout.children[1] = new_plot
 source_checkbox_button_group.on_change('active', callback)
 
-# data_new = df
-# data_new = pd.DataFrame.from_dict(dict(source = df.source, target = df.target, weight = df.weight))
 G = networkx.from_pandas_edgelist(df, 'source', 'target', edge_attr=['weight'], create_using=networkx.DiGraph())
+pos = networkx.spring_layout(G)
 # callback = CustomJS(args = {'checkbox':source_checkbox_button_group, 'df': df, 'data_new': data_new},
 # code = """
 # # console.log(df);
@@ -309,7 +227,6 @@ G = networkx.from_pandas_edgelist(df, 'source', 'target', edge_attr=['weight'], 
 #     df.data = data
 #     # create a callback that will reset the datasource
 
-
 ## Target
 target_cat = Div(text="Select Target Category")
 target_checkbox_button_group = CheckboxButtonGroup(labels=labels, active=[0, 1])
@@ -333,7 +250,7 @@ def target_callback(attr, old, new):
     # # new_data["y"] = new_y
     print("target", source)
     G = networkx.from_pandas_edgelist(source, 'source', 'target', edge_attr=['weight'], create_using=networkx.DiGraph())
-
+    
     #Calculate degree for each node and add as node attribute
     degrees = dict(networkx.degree(G))
     networkx.set_node_attributes(G, name='degree', values=degrees)
@@ -386,7 +303,6 @@ def target_callback(attr, old, new):
     label_source = ColumnDataSource({'x': x, 'y': y, 'name': [node_labels[i] for i in range(len(x))]})
     labels = LabelSet(x='x', y='y', text='name', source=label_source, background_fill_color='white', text_font_size='10px', background_fill_alpha=.8)
     new_plot.renderers.append(labels)
-    # show(column(group, plot))
     # output_file('network.html')
     #save(plot, filename=f"{title}.html")
     group = row(column(source_cat, source_select_all, source_select_none, source_checkbox_button_group),
@@ -396,13 +312,6 @@ def target_callback(attr, old, new):
 
 target_checkbox_button_group.on_change('active', callback)
 
-
-#Create a network graph object and filter
-# view1 = combined_df[combined_df["Category"].str.contains("Teaching")==True]
-# view2 = combined_df[combined_df["Category"].str.contains("R&D")==True]
-# view3 = combined_df[combined_df["Category"].str.contains("Organizer")==True]
-# view4 = combined_df[combined_df["Category"].str.contains("Knowledge")==True]
-# view5 = df[df["source_category"].str.contains("Media")==True]
 #Calculate degree for each node and add as node attribute
 degrees = dict(networkx.degree(G))
 networkx.set_node_attributes(G, name='degree', values=degrees)
@@ -425,15 +334,17 @@ for community_number, community in enumerate(communities):
         modularity_class[name] = community_number
         modularity_color[name] = Spectral8[community_number]
 
-# Add modularity class, color, URLs as attributes from the network above
+# Add modularity class, color, URLs, categoty, degree centrality as attributes from the network above
 networkx.set_node_attributes(G, modularity_class, 'modularity_class')
 networkx.set_node_attributes(G, modularity_color, 'modularity_color')
 networkx.set_node_attributes(G, url_class, "URL")
 networkx.set_node_attributes(G, category_class, "category")
+
 # Configure tap tool
 taptool = plot.select(type=TapTool)
 taptool.callback = OpenURL(url="@URL")
 network_graph = from_networkx(G, networkx.spring_layout, scale=15, center=(0, 0))
+
 #Create newnetwork graph from filtered data
 #Set node sizes and colors according to node degree (color as category from attribute)
 network_graph.node_renderer.glyph = Circle(size=size_by_this_attribute, fill_color=color_by_this_attribute)
@@ -459,10 +370,54 @@ node_labels = list(G.nodes())
 label_source = ColumnDataSource({'x': x, 'y': y, 'name': [node_labels[i] for i in range(len(x))]})
 labels = LabelSet(x='x', y='y', text='name', source=label_source, background_fill_color='white', text_font_size='10px', background_fill_alpha=.8)
 plot.renderers.append(labels)
-# print(plot)
-# show(column(group, plot))
-# output_file('network.html')
-#save(plot, filename=f"{title}.html")
+
+#Dropdown widget
+domains_sorted = sorted(df_cat['domains'])
+menu = list(zip(domains_sorted, domains_sorted))
+selection = Select(options=menu)
+def callback_select(attr, old, new):
+    selected_node=selection.value
+    print(selected_node)
+    shortest = networkx.shortest_path(G.to_undirected(),source=selected_node)
+    filtered = [k for k,v in shortest.items() if len(v)==2 or len(v)==1]
+    def filter_node(n):
+        return n in filtered
+    def filter_edge(n1, n2):
+        return n1 in source or n2 in source
+    G_node = networkx.subgraph_view(G, filter_node=filter_node, filter_edge=filter_edge)
+    network_graph=networkx.draw_networkx(G_node, pos=pos)
+    # network_graph = from_networkx(G_node, networkx.spring_layout, scale=15, center=(0, 0))
+    #Create newnetwork graph from filtered data
+    #Set node sizes and colors according to node degree (color as category from attribute)
+    network_graph.node_renderer.glyph = Circle(size=size_by_this_attribute, fill_color=color_by_this_attribute)
+    #Set node highlight colors
+    network_graph.node_renderer.hover_glyph = Circle(size=size_by_this_attribute, fill_color=node_highlight_color, line_width=2)
+    network_graph.node_renderer.selection_glyph = Circle(size=size_by_this_attribute, fill_color=node_highlight_color, line_width=2)
+    #Set edge opacity and width
+    network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.3, line_width=1)
+    #Set edge highlight colors
+    network_graph.edge_renderer.selection_glyph = MultiLine(line_color=edge_highlight_color, line_width=2)
+    network_graph.edge_renderer.hover_glyph = MultiLine(line_color=edge_highlight_color, line_width=2)
+    #Highlight nodes and edges
+    network_graph.selection_policy = NodesAndLinkedEdges()
+    network_graph.inspection_policy = NodesAndLinkedEdges()
+    plot.renderers.append(network_graph)
+    #Add Labels
+    x, y = zip(*network_graph.layout_provider.graph_layout.values())
+    node_labels = list(G_node.nodes())
+    label_source = ColumnDataSource({'x': x, 'y': y, 'name': [node_labels[i] for i in range(len(x))]})
+    labels = LabelSet(x='x', y='y', text='name', source=label_source, background_fill_color='white', text_font_size='10px', background_fill_alpha=.8)
+    plot.renderers.append(labels)
+    # show(column(group, plot))
+    # output_file('network.html')
+    #save(plot, filename=f"{title}.html")
+    group = row(column(source_cat, source_select_all, source_select_none, source_checkbox_button_group),
+                column(target_cat, target_select_all, target_select_none, target_checkbox_button_group),
+                sizing_mode='stretch_both')
+    layout.children[2] = plot
+selection.on_change('value', callback_select)
+
+#Remove school button
 remove_school_button = Button(label="Remove Schools",  button_type='danger')
 def callback_remove():
     new_plot = figure(tooltips = HOVER_TOOLTIPS,
@@ -470,15 +425,10 @@ def callback_remove():
               x_range=Range1d(-15.1, 15.1), y_range=Range1d(-15.1, 15.1), 
               plot_width=1000, 
               title=title)
-    cat_selected = ['Media', 'Knowledge', 'R&D', 'Organizer']
-    print("source",cat_selected)
-    source =  df[(df["target_category"].isin(cat_selected)) & (df['source_category'].isin(cat_selected))]
-    # # new_data = dict()
-    # # new_x = filtered["Year"].unique()
-    # # new_y = filtered.groupby("Year")["Value"].sum().values
-    # # new_data["x"] = new_x
-    # # new_data["y"] = new_y
-    print("updated",source)
+    cat_selected = 'Teaching'
+    df.dropna(inplace=True)
+    source =  df[df["target_category"].str.contains(cat_selected)==False]
+    source = source[source['source_category'].str.contains(cat_selected)==False]
     G = networkx.from_pandas_edgelist(source, 'source', 'target', edge_attr=['weight'], create_using=networkx.DiGraph())
 
     #Calculate degree for each node and add as node attribute
@@ -542,12 +492,66 @@ def callback_remove():
     layout.children[2] = new_plot
 
 remove_school_button.on_click(callback_remove)
+reset_button = Button(label="Reset",  button_type='danger')
+
+
+
 group = row(column(source_cat, source_select_all, source_select_none, source_checkbox_button_group),
             column(target_cat, target_select_all, target_select_none, target_checkbox_button_group),
             sizing_mode='stretch_both')
-layout = column(group, remove_school_button, plot)
-curdoc().add_root(layout)
+
+layout = column(group, row(remove_school_button,reset_button, selection,sizing_mode="scale_width"), plot)
+analysis_text = Div(text="Network Analysis", style={'font-size': '150%'})
+density_text = Div(text="Density:", style={'font-weight': 'bold'})
+density = Div(text="{:.2f}".format(networkx.density(G)))
+
+
+clustering_text = Div(text="Clustering Coefficient:", style={'font-weight': 'bold'})
+print(networkx.average_clustering(G))
+clustering = Div(text="{:.2f}".format(networkx.average_clustering(G)))
+
+degree = networkx.degree_centrality(G)
+in_degree = networkx.in_degree_centrality(G)
+out_degree = networkx.out_degree_centrality(G)
+eigen_degree = networkx.eigenvector_centrality(G)
+
+sorted_degree = sorted(degree.items(), key=itemgetter(1), reverse=True)
+sorted_in_degree = sorted(in_degree.items(), key=itemgetter(1), reverse=True)
+sorted_out_degree = sorted(out_degree.items(), key=itemgetter(1), reverse=True)
+sorted_eigen_degree = sorted(eigen_degree.items(), key=itemgetter(1), reverse=True)
+
+print("Top 20 nodes by degree:")
+degree_output = " "
+for n, v in sorted_degree[:5]:
+    degree_output += n + " " + "{:.2f}".format(v) + '</br>'
+degree_output_div = Div(text=str(degree_output))
+
+in_degree_output = " "
+for n, v in sorted_in_degree[:5]:
+    in_degree_output += n + " " + "{:.2f}".format(v) + '</br>'
+in_degree_output_div = Div(text=str(in_degree_output))
+out_degree_output = " "
+for n, v in sorted_out_degree[:5]:
+    out_degree_output += n + " " + "{:.2f}".format(v) + '</br>'
+out_degree_output_div = Div(text=str(out_degree_output))
+eigen_degree_output = " "
+for n, v in sorted_eigen_degree[:5]:
+    eigen_degree_output += n + " " + "{:.2f}".format(v) + '</br>'
+eigen_degree_output_div = Div(text=str(eigen_degree_output))
+
+degree_centrality_text = Div(text="Degree Centrality:")
+in_degree_centrality_text = Div(text="Top 5 In-degree Centrality:", style={'font-weight': 'bold'})
+out_degree_centrality_text = Div(text="Top 5 Out-degree Centrality:", style={'font-weight': 'bold'})
+eigen_degree_centrality_text = Div(text="Top 5 Eigenvector Centrality:", style={'font-weight': 'bold'})
+
+
+analysis = column(analysis_text, density_text, density, 
+                  clustering_text, clustering,
+                #   degree_centrality_text, degree_output_div,
+                  in_degree_centrality_text, in_degree_output_div,
+                  out_degree_centrality_text, out_degree_output_div,
+                  eigen_degree_centrality_text, eigen_degree_output_div)
+Title_text = Div(text= "Network of Innovation Units at Penn", style={'font-size': '200%'})
+dashboard = column(Title_text, row(layout, analysis))
+curdoc().add_root(dashboard)
 # https://towardsdatascience.com/how-to-create-a-plotly-visualization-and-embed-it-on-websites-517c1a78568b
-
-
-
